89445c126576ef40515c76c77b1c67b3dcb51e0c
Khush Jain <khush4080@gmail.com>
2020-08-06 17:59:15 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+const getNotes = () => {
+    return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.filter((note) => note.title === title)
+
+    if(duplicateNote.length === 0) {
+       
+        notes.push({
+            title: title,
+            body: body
+        })
+        
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch(e){
+        return []
+    }
+
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }
+
+}
+
+
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
